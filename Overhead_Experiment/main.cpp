4230af314e1a978ea52c09f8f7bedcf29c4d8c08
Nick McClellan <mcclelnr@noyce.whitman.edu>
2019-07-25 15:27:22 -0700

    too easy

--- Overhead_Experiment/main.cpp
+++ Overhead_Experiment/main.cpp
@@ -4,6 +4,8 @@
 #include "sim/determ/determ.hpp"
 #include "sim/stoch/fast_gillespie_direct_simulation.hpp"
 #include "sim/stoch/next_reaction_simulation.hpp"
+#include "sim/stoch/rejection_based_simulation.hpp"
+//#include "sim/stoch/Gillespie_Direct_Simulation.hpp"
 #include "model_impl.hpp"
 #include "Sim_Builder.hpp"
 #include "run_simulation.hpp"
@@ -47,14 +49,23 @@ int main(int argc, char* argv[]){
   simulate_experiment<Fast_Gillespie_Direct_Simulation>(ac, av, &args, "Overhead_Experiment/Fast_Gillespie_Overhead");
   std::cout << style::apply(Color::yellow) << "Finished the Fast Gillespie Simulation\n\n";
   
-  std::cout << "Starting the Deterministic Simulation\n" << style::reset();
-  simulate_experiment<Deterministic_Simulation>(ac, av, &args, "Overhead_Experiment/Deterministic_Overhead");
-  std::cout << style::apply(Color::yellow) << "Finished the Deterministic Simulation\n\n";
+//  std::cout << "Starting the Stochastic Simulation\n" << style::reset();
+//  simulate_experiment<Stochastic_Simulation>(ac, av, &args, "Overhead_Experiment/Stochastic_Overhead");
+//  std::cout << style::apply(Color::yellow) << "Finished the Stochastic Simulation\n\n" << style::reset();
   
-  std::cout << "Starting the Next Reaction Simulation\n" << style::reset();
+//  std::cout << "Starting the Anderson Next Reaction Simulation\n" << style::reset();
+//  simulate_experiment<Anderson_Next_Reaction_Simulation>(ac, av, &args, "Overhead_Experiment/Anderson_Overhead");
+//  std::cout << style::apply(Color::yellow) << "Finished the Anderson Next Reaction Simulation\n\n";
+  
+  std::cout << "Starting the Rejection Based Simulation\n" << style::reset();
+  simulate_experiment<Rejection_Based_Simulation>(ac, av, &args, "Overhead_Experiment/Rejection_Based_Overhead");
+  std::cout << style::apply(Color::yellow) << "Finished the Rejection Based Simulation\n\n" << style::reset();
+  
+  
+//  std::cout << "Starting the Next Reaction Simulation\n" << style::reset();
   //simulate_experiment<Next_Reaction_Simulation>(ac, av, &args, "Overhead_Experiment/Next_Reaction_Overhead");
-  std::cout << style::apply(Color::red) << "Error: could not run\n" << style::reset();
-  std::cout << style::apply(Color::yellow) << "Finished the Next Reaction Simulation\n" << style::reset();
+//  std::cout << style::apply(Color::red) << "Error: could not run\n" << style::reset();
+//  std::cout << style::apply(Color::yellow) << "Finished the Next Reaction Simulation\n" << style::reset();
 }
 
 //PARAMS USING: ./overhead_experiment -p Overhead_Experiment/param_sets.csv -e " " -d Overhead_Experiment/init_conc.csv -t 10 -u 1.0 -c 100 -w 1 -s 1.0
