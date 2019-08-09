6800eb470a26e36c8a9d7cd003c89caa6aaec6c8
Nick McClellan <46505547+NickRMcCellan@users.noreply.github.com>
2019-08-09 10:22:23 -0700

    Update graphmaker.py

--- graphmaker.py
+++ graphmaker.py
@@ -11,52 +11,57 @@ def autolabel(rects, a):
                     xy=(rect.get_x() + rect.get_width() / 2, height),
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
-                    ha='center', va='bottom')
+                    ha='center', va='bottom',size=5,rotation=90)
 
 def main():
   sim_names = []
   sim_data = []
-  
-  with open(sys.argv[1]) as csvfile:
-    graph_data = csv.reader(csvfile, delimiter=",")
-    line_count = 0
-    for row in graph_data:
-      if line_count == 0:
-        sim_names = row
-        line_count += 1
-      else:
-        sim_data.append(row)
-        line_count +=1
-      print(f'processed {line_count} line(s).')
-  x = np.arange(len(sim_names))
-  width = 0.15
+  sim_experiments = []
+  for file in sys.argv[1:-1]:
+    with open(file) as csvfile:
+      sim_experiments.append(file[:-16])
+      graph_data = csv.reader(csvfile, delimiter=",")
+      line_count = 0
+      for row in graph_data:
+        if line_count == 0:
+          sim_names = row
+          line_count += 1
+        else:
+          sim_data.append(row)
+          line_count +=1
+  x = np.arange(0,14*len(sim_experiments),14)
+  width = 2
   
   sim_data_nums  = []
-  for i in range(len(sim_data)):
+  for i in sim_data:
     temp = []
-    for j in range(len(sim_data[i])):
-      temp.append(float(sim_data[i][j]))
+    for j in i:
+      temp.append(float(j))
     sim_data_nums.append(temp)
   
   
-  
   fig, ax = plt.subplots()
-  rects = [] 
-  num_data =  ((len(sim_data_nums)/4)*-1)*width
+  rects = []
+  
+  data = []
+  
+  num_data =  -5
+  for i in range(len(sim_data_nums[0])):
+    temp = []
+    for j in range(len(sim_data_nums)):
+      temp.append(sim_data_nums[j][i])
+    data.append(temp)
   count = 1
-  for d in sim_data_nums:
-    if (num_data == 0):
-      div = 0
-    else:  
-      div = width/num_data
-    rects1 = ax.bar(x + num_data, d, width, label=f'Entry {count}')
+  
+  for d in data:
+    rects1 = ax.bar(x + num_data, d, width, label= sim_names[count - 1])
     rects.append(rects1)
     num_data += width
     count += 1
   ax.set_ylabel('Reactions/Second')
-  ax.set_title(sys.argv[2])
+  ax.set_title(sys.argv[-1])
   ax.set_xticks(x)
-  ax.set_xticklabels(sim_names)
+  ax.set_xticklabels(sim_experiments)
   ax.legend()
   
   for r in rects:
@@ -68,6 +73,6 @@ def main():
   plt.show()
   
 main()
-  
-  
 
+#Command line arguements:
+#python3 graphmaker.py Exponential_performance.csv Conc_performance.csv Density_performance.csv Oscillating_performance.csv Scale_performance.csv Overhead_performance.csv "Data"
